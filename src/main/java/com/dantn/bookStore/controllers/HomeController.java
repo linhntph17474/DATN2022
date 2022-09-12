7fc9a007effdc3f494e7c5ed0d25b4c545e33439
phuthai02 <doanphuthai1152002@gmail.com>
2022-09-12 12:50:36 +0700

    index template

--- src/main/java/com/dantn/bookStore/controllers/HomeController.java
+++ src/main/java/com/dantn/bookStore/controllers/HomeController.java
@@ -1,5 +1,13 @@
 package com.dantn.bookStore.controllers;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";	
+	}
 }
